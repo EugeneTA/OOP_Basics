fce72ee40590b21ca78a23a76b7f1eb078939a85
EugeneTA <tishkov2005@yandex.ru>
2022-03-31 19:13:48 +0300

    Lesson_04

---
+++ Lesson_04/Lesson_04_Part_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lesson_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.Реализовать класс для описания здания(уникальный номер здания, высота, этажность, количество квартир, подъездов).
+            // Поля сделать закрытыми, предусмотреть методы для заполнения полей и получения значений полей для печати.
+            // Добавить методы вычисления высоты этажа, количества квартир в подъезде, количества квартир на этаже и т.д.
+            // Предусмотреть возможность, чтобы уникальный номер здания генерировался программно.
+            // Для этого в классе предусмотреть статическое поле, которое бы хранило последний использованный номер здания,
+            // и предусмотреть метод, который увеличивал бы значение этого поля.
+
+            Building concreteBuilding = new ConcreteBuilding(50, 20, 120, 2);
+            Building brickBuilding = new BrickBuilding(20, 5, 40, 4);
+            Building woodenBuilding3 = new WoodenBuilding(12, 3, 6, 1);
+
+            // Выводим информацию по созданным зданиям
+
+            concreteBuilding.PrintBuildingInfo();
+            brickBuilding.PrintBuildingInfo();
+            woodenBuilding3.PrintBuildingInfo();
+
+            // Выводим значение глобального индекса зданий
+            Console.WriteLine(GlobalBuildingIndex.ToString());
+
+            Console.WriteLine();
+            Console.WriteLine(" Нажмите любую клавишу для завершения работы ...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson_04/Lesson_04_Part_2_and_3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using BuildingLibrary;
+
+namespace Lesson_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 2. * Для реализованного класса создать новый класс Creator, который будет являться фабрикой объектов класса здания.
+            // Для этого изменить модификатор доступа к конструкторам класса,
+            // в новый созданный класс добавить перегруженные фабричные методы CreateBuild для создания объектов класса здания.
+            // В классе Creator все методы сделать статическими, конструктор класса сделать закрытым.
+            // Для хранения объектов класса здания в классе Creator использовать хеш-таблицу.
+            // Предусмотреть возможность удаления объекта здания по его уникальному номеру из хеш-таблицы.
+            // Создать тестовый пример, работающий с созданными классами.
+
+            // 3. * Разбить созданные классы(здания, фабричный класс) и тестовый пример в разные исходные файлы.
+            // Разместить классы в одном пространстве имен. Создать сборку(DLL), включающие эти классы.
+            // Подключить сборку к проекту и откомпилировать тестовый пример со сборкой.
+            // Получить исполняемый файл, проверить с помощью утилиты ILDASM, что тестовый пример ссылается на сборку и не содержит в себе классов здания и Creator.
+
+            Building concreteBuilding = BuildingFactory.CreateConcreteBuilding(50, 20, 120, 2);
+            Building brickBuilding = BuildingFactory.CreateBrickBuilding(20, 5, 40, 4);
+            Building woodenBuilding3 = BuildingFactory.CreateWodenBuilding(12, 3, 6, 1);
+
+            // Выводим информацию по зданиям из базы
+            BuildingFactory.PrintBuildingsDatabase();
+
+            // Удаляем один из домов из базы
+            Console.WriteLine();
+            Console.WriteLine($" Удаляем здание с индексом: {brickBuilding.BuildingIndex}");
+            BuildingFactory.DeleteBuildingFromDatabase(brickBuilding);
+            Console.WriteLine();
+
+
+            // Выводим информацию по зданиям в базе еще раз
+            Console.WriteLine();
+            Console.WriteLine(" В базе остались следующие здания:");
+            Console.WriteLine();
+            BuildingFactory.PrintBuildingsDatabase();
+
+
+            Console.WriteLine();
+            Console.WriteLine(" Нажмите любую клавишу для завершения работы ...");
+            Console.ReadKey();
+        }
+    }
+}
